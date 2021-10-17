0d942668c624bb4fec2d4cb9e67ab0d45a605bb4
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2021-10-17 04:55:07 -1200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function resolveObject(object, el, p = false) {
+function resolveObject(object, el) {
     Object.keys(object).forEach(function (element, index) {
         var value = object[element];
 
@@ -15,15 +15,14 @@ function resolveObject(object, el, p = false) {
 
             var DIV_VALUE = document.createElement("div");
 
-            DIV_VALUE.setAttribute("class", "object-value");
-            DIV_VALUE.style = "margin-left: 10px;border-left: solid 2px;padding: 10px 0;padding-left: 8px;border-radius: 0px;border-color: #2196f3;";
+            DIV_VALUE.classList.add("object-value");
 
             DIV_CONTAINER.append(DIV_VALUE);
             el.append(DIV_CONTAINER);
 
             resolveObject(value, DIV_VALUE, true);
         } else {
-            el.innerHTML += "<p style='margin: 5px 0;'><b>" + element + ": </b>" + value + "</p>";
+            el.innerHTML += "<p id='object-key'><b>" + element + ": </b>" + value + "</p>";
         }
     });
 
@@ -39,5 +38,12 @@ function resolveObject(object, el, p = false) {
 
 
 function preview() {
-	resolveObject(JSON.parse(document.querySelector("textarea").value), document.querySelector(".json-preview"));
+    var JSON_PREVIEW = document.querySelector(".json-preview");
+
+    JSON_PREVIEW.innerHTML = "";
+    
+	resolveObject(
+        JSON.parse(document.querySelector("textarea").value), 
+        JSON_PREVIEW
+    );
 }
